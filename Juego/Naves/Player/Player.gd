ecbe0f7bec60bd2e9a196dc749b62189d3131698
AgustinAnriquez <agustin.anriquez98@gmail.com>
2022-07-14 14:49:37 -0300

    Canion, eventos, proyectiles
    Se agrego canion, disparo principal y secundario. Efectos especiales, se creo nuevo bus

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -10,11 +10,20 @@ export var potencia_rotacion:int = 280
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
 
+## Atributos Onready
+onready var canion:Canion = $Canion
+onready var laser:RayoLaser = $LaserBeam2D
+
 ## Metodos
+func _unhandled_input(event: InputEvent) -> void:
+	# Dispara Rayo
+	if event.is_action_pressed("disparo_secundario"):
+		laser.set_is_casting(true)
+
+	if event.is_action_released("disparo_secundario"):
+		laser.set_is_casting(false)
+
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
-	#Los prints son solo para entender mejor que esta pasando. 
-	print(rad2deg(rotation))
-	print(empuje.rotated(rotation))
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
 
@@ -35,3 +44,10 @@ func player_input() -> void:
 		dir_rotacion -= 1
 	elif Input.is_action_pressed("rotar_horario"):
 		dir_rotacion += 1
+	# Disparo
+	if Input.is_action_pressed("disparo_principal"):
+		canion.set_esta_disparando(true)
+	if Input.is_action_just_released("disparo_principal"):
+		canion.set_esta_disparando(false)
+
+
---
+++ Juego/Niveles/Nivel.gd
@@ -0,0 +1,23 @@
+#Nivel.gd
+class_name Nivel
+extends Node2D
+
+## Atributos Onready
+onready var contenedor_proyectiles:Node
+
+## Metodos
+func _ready() -> void:
+	conectar_seniales()
+	crear_contenedores()
+
+## Metodos Custom
+func conectar_seniales() -> void:
+	Eventos.connect("disparo", self, "_on_disparo")
+
+func crear_contenedores() -> void:
+	contenedor_proyectiles = Node.new()
+	contenedor_proyectiles.name = "ContenedorProyectiles"
+	add_child(contenedor_proyectiles)
+
+func _on_disparo(proyectil:Proyectil) -> void:
+	contenedor_proyectiles.add_child(proyectil)
