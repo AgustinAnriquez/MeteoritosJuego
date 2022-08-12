6c084a5e53960e64665ae88067ddbf3ae2745f2c
AgustinAnriquez <agustin.anriquez98@gmail.com>
2022-08-12 15:01:36 -0300

    se agrega enemigos orbitales alrededor de base enemiga

--- Juego/Estaciones/BaseEnemiga.gd
+++ Juego/Estaciones/BaseEnemiga.gd
@@ -5,17 +5,22 @@ extends Node2D
 ## Atributos Export
 export var hitpoints:float = 30.0
 export var orbital:PackedScene =null
+export var numero_orbitales:int = 10
+export var intervalo_spawn:float = 0.8
 
 ## Atributos Onready
 onready var impacto_sfx:AudioStreamPlayer2D = $ImpactoSFX
+onready var timer_spawner:Timer = $TimerSpawnerEnemigos
 
 ## Atributos
 var esta_destruida:bool = false
+var posicion_spawn:Vector2 = Vector2.ZERO
 
 ## Metodos
 func _ready() -> void:
+	timer_spawner.wait_time = intervalo_spawn
 	$AnimationPlayer.play(elegir_animacion_aleatoria())
-
+	
 func _process(delta: float) ->void:
 	var player_objetivo:Player = DatosJuego.get_player_actual()
 	if not player_objetivo:
@@ -52,11 +57,14 @@ func destruir() -> void:
 	queue_free()
 
 func spawnear_orbital() -> void:
-	var pos_spawn:Vector2 = deteccion_cuadrante()
+	numero_orbitales -=1
+	$RutaEnemigo.global_position = global_position
+	
 	var new_orbital:EnemigoOrbital = orbital.instance()
 	new_orbital.crear(
-		global_position + pos_spawn,
-		self
+		global_position + posicion_spawn,
+		self,
+		$RutaEnemigo
 	)
 	Eventos.emit_signal("spawn_orbital", new_orbital)
 
@@ -71,17 +79,21 @@ func deteccion_cuadrante() ->Vector2:
 	
 	if abs(angulo_player) <= 45.0:
 		# Player entra por la derecha
+		$RutaEnemigo.rotation_degrees = 180.0
 		return $PosicionesSpawn/Este.position
 	elif abs(angulo_player) > 135.0 and abs(angulo_player)<= 180.0:
 		# Player entra por la izquierda
+		$RutaEnemigo.rotation_degrees = 0.0
 		return $PosicionesSpawn/Oeste.position
 	elif abs(angulo_player) > 45.0 and abs(angulo_player) <= 135.0:
 		# Player entra por arriba o por abajo
 		if sign(angulo_player) > 0:
 			#Player entra por abajo
+			$RutaEnemigo.rotation_degrees = 270.0
 			return $PosicionesSpawn/Sur.position
 		else:
 			#Player entra por arriba
+			$RutaEnemigo.rotation_degrees = 90.0
 			return $PosicionesSpawn/Norte.position
 	
 	return $PosicionesSpawn/Norte.position
@@ -93,4 +105,12 @@ func _on_AreaColision_body_entered(body):
 
 func _on_VisibilityNotifier2D_screen_entered():
 	$VisibilityNotifier2D.queue_free()
+	posicion_spawn = deteccion_cuadrante()
+	spawnear_orbital()
+	timer_spawner.start()
+
+func _on_TimerSpawnerEnemigos_timeout():
+	if numero_orbitales == 0:
+		timer_spawner.stop()
+		return
 	spawnear_orbital()
--- Juego/Naves/Enemigo/EnemigoOrbital.gd
+++ Juego/Naves/Enemigo/EnemigoOrbital.gd
@@ -1,27 +1,39 @@
-#enemigoOrbital.gd
+#EnemigoOrbital.gd
 class_name EnemigoOrbital
 extends EnemigoBase
 
 ## Atributos Export
 export var rango_max_ataque: float = 1400.0
+export var velocidad:float = 400.0
+
+## Atributos Onready
+onready var detector_obstaculo:RayCast2D = $DetectorObstaculo
 
 ## Atributos
 var base_duenia:Node2D
+var ruta:Path2D
+var path_follow:PathFollow2D
 
 ## Constructor
-func crear(pos:Vector2, duenia: Node2D) -> void:
+func crear(pos:Vector2, duenia: Node2D, ruta_duenia:Path2D) -> void:
 	global_position = pos
 	base_duenia = duenia
+	ruta = ruta_duenia
+	path_follow = PathFollow2D.new()
+	ruta.add_child(path_follow)
 
 ## Metodos
 func _ready():
 	Eventos.connect("base_destruida", self, "_on_base_destruida")
-	canion.set_esta_disparando(true)
-	
+
+func _process(delta: float) -> void:
+	path_follow.offset += velocidad * delta
+	position = path_follow.global_position
+
 ## Metodos custom
 func rotar_hacia_player() -> void:
 	.rotar_hacia_player()
-	if dir_player.length() > rango_max_ataque:
+	if dir_player.length() > rango_max_ataque or detector_obstaculo.is_colliding():
 		canion.set_esta_disparando(false)
 	else:
 		canion.set_esta_disparando(true)
